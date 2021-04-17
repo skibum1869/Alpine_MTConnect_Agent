8ef683f3ea4e6cf44c9a6374feeff2a8e316f600
skibum1869 <sk18um@protonmail.com>
2021-04-17 13:12:15 -0500

    Security Test 8

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Run file to call the agent
 # cd /home/app/MTC_Agent/
-/lib/ld-musl-x86_64.so.1 --library-path lib /home/app/MTC_Agent/agent agent.cfg
+/lib/ld-musl-x86_64.so.1 --library-path lib /home/app/MTC_Agent/agent /home/app/MTC_Agent/agent.cfg
