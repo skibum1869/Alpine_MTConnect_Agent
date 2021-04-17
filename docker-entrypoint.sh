6b1c500d550dcbc0a3d113e579f82efd620f7792
skibum1869 <sk18um@protonmail.com>
2021-04-17 13:16:07 -0500

    fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # Run file to call the agent
 # cd /home/app/MTC_Agent/
-ls
-/lib/ld-musl-x86_64.so.1 --library-path lib /home/app/MTC_Agent/agent /home/app/MTC_Agent/agent.cfg
+/lib/ld-musl-x86_64.so.1 --library-path lib agent agent.cfg
